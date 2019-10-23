b24006045e7739d8e0f67f4a13cb1425b3986d36
PAL-2 <chenakeshava1@gmail.com>
2019-10-23 10:21:03 +0530

    Interact with the database

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -30,6 +31,9 @@ namespace PalTracker
             services.AddSingleton(sp => new WelcomeMessage(
               Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")));
 
+              services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+              services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+
               services.AddSingleton(sp => new CloudFoundryInfo(
               
               Configuration.GetValue<string>("PORT","Port not configured"),
@@ -39,7 +43,7 @@ namespace PalTracker
 
               ));
 
-              services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+              
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
